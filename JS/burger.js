2eaeb37a829351b6e328ec79111e48d5f88c1da9
Saksham <2020UCP1801@mnit.ac.in>
2021-09-17 16:26:21 +0530

    hamburger

---
+++ JS/burger.js
@@ -0,0 +1,8 @@
+// Getting Elements
+const burger = document.getElementById("burger");
+const navbar = document.getElementById("navbar");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+  navbar.style.maxHeight = navbar.style.maxHeight === "100%" ? "7vh" : "100%";
+});
